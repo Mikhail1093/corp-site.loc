1a963cebc01e3f6a0f7b8146e7f3e720d9716ee4
grishi <mihailg1093@gmail.com>
2017-10-15 14:56:27 +0300

    are added migration for tokens

---
+++ database/migrations/2017_10_15_144838_UserApiTokens.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserApiTokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key')->unique();
+            $table->integer('is_active')->default(1);
+            $table->timestamps();
+            $table->integer('user_id')->unsigned()->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tokens', function (Blueprint $table) {
+            //
+        });
+    }
+}
